c8f70096f602944e95bbfb340901a0618f7fb5aa
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-25 15:41:14 +0200

    refactoring

--- my_script.js
+++ my_script.js
@@ -3,12 +3,12 @@ const conSquare = document.getElementById("container_square")
 
 for (let i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        conSquare.innerHTML += `<div class="square_fizzBuzz">FizzBuzz</div>`
+        conSquare.innerHTML += `<div class="square square_fizzBuzz">FizzBuzz</div>`
     }else if (i % 5 == 0) {
-        conSquare.innerHTML += `<div class="square_buzz">Buzz</div>`
+        conSquare.innerHTML += `<div class="square square_buzz">Buzz</div>`
     }else if (i % 3 == 0) {
-        conSquare.innerHTML += `<div class="square_fizz">Fizz</div>`
+        conSquare.innerHTML += `<div class="square square_fizz">Fizz</div>`
     }else {
-        conSquare.innerHTML += `<div class="square"> ${i}</div>`
+        conSquare.innerHTML += `<div class="square squareBasic"> ${i}</div>`
     } 
 }
